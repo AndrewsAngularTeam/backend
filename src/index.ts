4717d4b54a435ee6b413fa725e3ef73af666a45b
BobTheSoftwareDeveloper <48901347+BobTheSoftwareDeveloper@users.noreply.github.com>
2022-07-23 14:06:07 +1200

    Add middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from 'express'
 import { leaderboardRouter } from './routes'
+import morgan from 'morgan'
+import cors from 'cors'
 
 import dotenv from 'dotenv'
 dotenv.config()
 const port = process.env.PORT
 
 const app = express()
+app.use(morgan('tiny'))
+app.use(cors())
 
 app.get('/', async (req: Request, res: Response) => {
   return res.status(200).send('Hello :)')
